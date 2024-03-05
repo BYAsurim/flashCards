885a0de3a7bc51701aa57ef9ea3c603de05eea4a
BYAsurim <Pers0naL276276@gmail.com>
2024-03-05 13:36:21 +0300

    add Checkbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import { Button } from '@/components/ui/button'
+import { useState } from 'react'
+
+import { Button } from '@/components/ui/button/Button'
+import { Checkbox } from '@/components/ui/checkbox/Checkbox'
 
 export function App() {
+  const [value, setValue] = useState(false)
+  const foo = () => {
+    setValue(!value)
+  }
+
   return (
-    <div>
+    <div style={{ display: 'flex', gap: '20px', marginTop: '50px' }}>
       <Button> Hello</Button>
       <Button as={'a'} href={'https://www.google.by/'}>
         link
       </Button>
+      <Checkbox checked={value} label={'Check-box'} onChange={foo} />
     </div>
   )
 }
